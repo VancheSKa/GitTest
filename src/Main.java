175419decaf6e47af6c4f22255f106d6b95121e8
VancheSKa <69359310+VancheSKa@users.noreply.github.com>
2020-10-18 19:30:41 +0300

    first commit

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,20 @@
 public class Main {
+    String name;
+    int age;
+
+    public Main(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
     public static void main(String[] args) {
         String s = "Hello world!";
         System.out.println(s);
+
+        printName();
+    }
+    public static void printName() {
+        String name = "Stepan";
+        int age = 32;
+        System.out.println(name + " " + age);
     }
 }
